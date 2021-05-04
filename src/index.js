464d2db3b6e79386de3bbe7fa7e47d627acb9719
Basith Kunimal <basith@intellicar.in>
2021-05-04 08:48:08 +0530

    tests pass

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import styles from './styles.module.css'
 import iro from '@jaames/iro'
 
 export const ColorPicker = ({ setColor, color }) => {
-  const ref = useRef();
-  const colorPicker = useRef();
+  const ref = useRef()
+  const colorPicker = useRef()
   useEffect(() => {
-    let cp = colorPicker.current = new iro.ColorPicker(ref.current, {
+    const cp = (colorPicker.current = new iro.ColorPicker(ref.current, {
       color
-    });
+    }))
     cp.on('color:change', (color) => setColor(color.hexString))
-  }, []);
-  return <div className={styles.container} ref={ref}>
-  </div>
+  }, [])
+  return <div className={styles.container} ref={ref} />
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import { ColorPicker } from '.'
 
-describe('ExampleComponent', () => {
+describe('ColorPicker', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(ColorPicker).toBeTruthy()
   })
 })
